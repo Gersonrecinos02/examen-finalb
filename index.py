e06f6dbda073b906dd1ac14e996ca3f14c464da5
gersonrecinos02 <gersonenriquerecinoslemus@gmail.com>
2020-06-06 14:55:19 -0600

    se arreglaron algunos errores

--- index.py
+++ index.py
@@ -1,42 +1,39 @@
 from tkinter import *
 
 root = Tk()
-root.title("Examen final")
-root.geometry("600x500")
 
-miFrame = Frame()
-miFrame.pack()
+ancho = 460
+alto = 250
 
-Saludo = Label(text="Bienvenidos",font=("Agency6 FB",25)).place(x=250,y=30)
+root.geometry(str(ancho)+"x"+str(alto))
+root.title("FINAL")
 
-def Resultado():
-    Label(root,text=""+Nombre()+' es impar y '+Apellido()+'es impar',font=("Agency Fb",14)).place(x=25,y=120)
-    
+saludo = Label(text="Bienvenido",font=("Agency FB",14)).place(x=150,y=5)
 
-Nombre= Label(text="Nombre",font=("Agency FB",14)).place(x=25,y=80)
-Nombre=StringVar()
-txtnombre=Entry(root,textvariable=Nombre,width=34)
+lblname=Label(text="Nombre",font=("Agency FB",14)).place(x=80,y=30)
+entrada=StringVar()
+txtnombre=Entry(root,textvariable=entrada).place(x=135,y=40)
 
-Apellido= Label(text="Apellido",font=("Agency FB",14)).place(x=25,y=120)
-Apellido=StringVar()
-txtapellido=Entry(root,textvariable=Apellido)
+lblape=Label(text="Apellido",font=("Agency FB",14)).place(x=80,y=60)
+entrada=StringVar()
+txtape=Entry(root,textvariable=entrada).place(x=135,y=70)
 
-Día= Label(text="Día",font=("Agency FB",14)).place(x=25,y=160)
-Día=StringVar()
-txdia=Entry(root,textvariable=Nombre,)
+lbldia=Label(text="Dia",font=("Agency FB",14)).place(x=80,y=90)
+entrada=StringVar()
+txtdia=Entry(root,textvariable=entrada).place(x=135,y=100)
 
-Mes = Label(text="Mes",font=("Agency FB",14)).place(x=25,y=220)
-Mes=StringVar()
-txtmes=Entry(root,textvariable=Nombre)
+lblmes=Label(text="Mes",font=("Agency FB",14)).place(x=80,y=120)
+entrada=StringVar()
+txtmes=Entry(root,textvariable=entrada).place(x=135,y=130)
 
-Año= Label(text="Año",font=("Agency FB",14)).place(x=25,y=260)
-Año=StringVar()
-txtaño=Entry(root,textvariable=Nombre)
+lblaño=Label(text="Año",font=("Agency FB",14)).place(x=80,y=150)
+entrada=StringVar()
+txtaño=Entry(root,textvariable=entrada).place(x=135,y=160)
 
-btnFuntion1 = Button(root, text= "FUNCION 1")
-btnFuntion1 = Button(root, text= "FUNCION 2")
-btnFuntion1 = Button(root, text= "FUNCION 3")
-btnFuntion1 = Button(root, text= "FUNCION 4")
-btnFuntion1 = Button(root, text= "FUNCION 5")
+btnFuncion1 = Button(root, text= "FUNCION 1",font=("Agency FB",10),width=10).place(x=80,y=180)
+btnFuncion1 = Button(root, text= "FUNCION 2",font=("Agency FB",10),width=10).place(x=130,y=180)
+btnFuncion1 = Button(root, text= "FUNCION 3",font=("Agency FB",10),width=10).place(x=180,y=180)
+btnFuncion1 = Button(root, text= "FUNCION 4",font=("Agency FB",10),width=10).place(x=230,y=180)
+btnFuncion1 = Button(root, text= "FUNCION 5",font=("Agency FB",10),width=10).place(x=280,y=180)
 
 root.mainloop()
