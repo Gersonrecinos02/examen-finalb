b3cf465e96a02b8c005bcd8f9d0cc06d3d6ec58f
gersonrecinos02 <gersonenriquerecinoslemus@gmail.com>
2020-06-06 09:09:38 -0600

    Se arregla los errores

--- index.py
+++ index.py
@@ -5,13 +5,9 @@ root.title("Examen final")
 root.geometry("600x500")
 
 miFrame = Frame()
-
 miFrame.pack()
 
-
 Saludo = Label(text="Bienvenidos",font=("Agency6 FB",25)).place(x=250,y=30)
-
-
 
 def Resultado():
     Label(root,text=""+Nombre()+' es impar y '+Apellido()+'es impar',font=("Agency Fb",14)).place(x=25,y=120)
