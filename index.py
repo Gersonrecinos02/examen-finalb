605d14654a7ffcb73f302f3505322f1dfdc34ea6
gersonrecinos02 <gersonenriquerecinoslemus@gmail.com>
2020-06-06 08:50:59 -0600

    Se creo un label de tipo nombre

--- index.py
+++ index.py
@@ -17,6 +17,11 @@ def Resultado():
     Label(root,text=""+Nombre()+' es impar y '+Apellido()+'es impar',font=("Agency Fb",14)).place(x=25,y=120)
     
 
+Nombre= Label(text="Nombre",font=("Agency FB",14)).place(x=25,y=80)
+Nombre=StringVar()
+txtnombre=Entry(root,textvariable=Nombre,width=34)
+
+
 
 
 
